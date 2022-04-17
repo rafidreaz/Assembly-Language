8646366da33092829b9a4933adf0ccac776a8e30
rafidreaz <md.rafid.reaz@g.bracu.ac.bd>
2022-04-18 00:44:08 +0600

    Create loop keyword.asm

---
+++ loop keyword.asm
@@ -0,0 +1,19 @@
+include 'EMU8086.INC'
+.model small
+.stack 100h
+.data
+    a db ?
+.code
+main proc
+    ;for (int i = n; i>=0;i--)
+    mov cx, 10; will run the loop for 10 times
+    
+    top:
+    printn "loop is running"
+    
+    loop top;i--;i>=0
+    printn "loop has ended "
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
